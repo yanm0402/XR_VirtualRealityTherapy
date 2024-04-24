2add44a5ac9237d06c5ed2c6fe9f5406816445e8
Dharini Chandrashekar <dharinic@umd.edu>
2024-04-24 12:29:51 -0400

    Task Scene updates

---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust this to control the speed
+    public OVRCameraRig cameraRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from the thumbstick
+        Vector2 thumbstickInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        
+        // Calculate the movement direction based on the thumbstick input
+        Vector3 moveDirection = new Vector3(thumbstickInput.x, 0f, thumbstickInput.y);
+        moveDirection = transform.TransformDirection(moveDirection); // Transform relative to the camera's orientation
+        
+        // Move the camera position
+        Quaternion rotate = cameraRig.centerEyeAnchor.rotation;
+        transform.position += rotate * moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
--- Assets/scripts/cameracontroller.cs
+++ Assets/scripts/cameracontroller.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameracontroller : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public float speed = 5.0f;
-public float sensitivity = 5.0f;
+    public float cameraspeed = 5.0f;
+    public float sensitivity = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,10 @@ public float sensitivity = 5.0f;
     // Update is called once per frame
     void Update()
     {
-       transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-    transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-    float mouseX = Input.GetAxis("Mouse X");
-    float mouseY = Input.GetAxis("Mouse Y");
-    transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
-
+        transform.position += transform.forward * Input.GetAxis("Vertical") * cameraspeed * Time.deltaTime;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * cameraspeed * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
     }
 }
